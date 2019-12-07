77e0925b344625cdbe33264051cbda62e1bdd1c0
vermin <vermin@techie.com>
2019-12-07 23:51:28 +0100

    wazn way :evergreen_tree:

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
+# Why node:8 and not node:10? Because (a) v8 is LTS, so more likely to be stable, and (b) "npm update" on node:10 breaks on Docker on Linux (but not on OSX, oddly)
+FROM node:8-slim
+
 RUN apt-get update \
   && DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs-legacy npm git libboost1.55-all libssl-dev \
   && rm -rf /var/lib/apt/lists/* && \
@@ -12,11 +15,8 @@ RUN mkdir -p /config
 
 EXPOSE 8117
 EXPOSE 3333
-EXPOSE 4444
 EXPOSE 5555
 EXPOSE 7777
-EXPOSE 9099
-EXPOSE 6379
 
 VOLUME ["/config"]
 
--- init.js
+++ init.js
@@ -1,4 +1,3 @@
-/* jshint esversion: 6 */
 /**
  * Cryptonite Node.JS Pool
  * https://github.com/dvandal/cryptonote-nodejs-pool
@@ -23,21 +22,11 @@ var redis = require('redis');
 var redisDB = (config.redis.db && config.redis.db > 0) ? config.redis.db : 0;
 global.redisClient = redis.createClient(config.redis.port, config.redis.host, { db: redisDB, auth_pass: config.redis.auth });
 
-if ((typeof config.poolServer.mergedMining !== 'undefined' && config.poolServer.mergedMining) && typeof config.childPools !== 'undefined')
-    config.childPools = config.childPools.filter(pool => pool.enabled);
-else
-    config.childPools = [];
 // Load pool modules
 if (cluster.isWorker){
     switch(process.env.workerType){
         case 'pool':
             require('./lib/pool.js');
-            break;
-        case 'daemon':
-            require('./lib/daemon.js');
-            break;
-        case 'childDaemon':
-            require('./lib/childDaemon.js');
             break;
         case 'blockUnlocker':
             require('./lib/blockUnlocker.js');
@@ -63,8 +52,8 @@ var logSystem = 'master';
 require('./lib/exceptionWriter.js')(logSystem);
 
 // Pool informations
-log('info', logSystem, 'Starting WAZN Node.JS pool version %s', [version]);
-
+log('info', logSystem, 'Starting Cryptonote Node.JS pool version %s', [version]);
+ 
 // Run a single module ?
 var singleModule = (function(){
     var validModules = ['pool', 'api', 'unlocker', 'payments', 'chartsDataCollector', 'telegramBot'];
@@ -90,9 +79,6 @@ var singleModule = (function(){
             log('info', logSystem, 'Running in single module mode: %s', [singleModule]);
 
             switch(singleModule){
-                case 'daemon':
-                    spawnDaemon();
-                    break;
                 case 'pool':
                     spawnPoolWorkers();
                     break;
@@ -115,9 +101,6 @@ var singleModule = (function(){
         }
         else{
             spawnPoolWorkers();
-            spawnDaemon();
-	    if (config.poolServer.mergedMining)
-   	        spawnChildDaemons();
             spawnBlockUnlocker();
             spawnPaymentProcessor();
             spawnApi();
@@ -171,8 +154,17 @@ function spawnPoolWorkers(){
         log('error', logSystem, 'Pool server enabled but no ports specified');
         return;
     }
-    let numForks = config.childPools.length;
-    if (numForks === 0) return;
+
+    var numForks = (function(){
+        if (!config.poolServer.clusterForks)
+            return 1;
+        if (config.poolServer.clusterForks === 'auto')
+            return os.cpus().length;
+        if (isNaN(config.poolServer.clusterForks))
+            return 1;
+        return config.poolServer.clusterForks;
+    })();
+
     var poolWorkers = {};
 
     var createPoolWorker = function(forkId){
@@ -210,92 +202,6 @@ function spawnPoolWorkers(){
             log('info', logSystem, 'Pool spawned on %d thread(s)', [numForks]);
         }
     }, 10);
-}
-
-/**
- * Spawn pool workers module
- **/
-function spawnChildDaemons(){
-    if (!config.poolServer || !config.poolServer.enabled || !config.poolServer.ports || config.poolServer.ports.length === 0) return;
-
-    if (config.poolServer.ports.length === 0){
-        log('error', logSystem, 'Pool server enabled but no ports specified');
-        return;
-    }
-
-    var numForks = (function(){
-        if (!config.poolServer.mergedMining)
-            return 0;
-        if (typeof config.childPools !== 'undefined') {
-	    return config.childPools.length;
-	}
-        return 0;
-    })();
-    var daemonWorkers = {};
-
-    var createDaemonWorker = function(poolId){
-        var worker = cluster.fork({
-            workerType: 'childDaemon',
-            poolId: poolId
-        });
-        worker.poolId = poolId;
-        worker.type = 'childDaemon';
-        daemonWorkers[poolId] = worker;
-        worker.on('exit', function(code, signal){
-            log('error', logSystem, 'Child Daemon fork %s died, spawning replacement worker...', [poolId]);
-            setTimeout(function(){
-                createDaemonWorker(poolId);
-            }, 2000);
-        }).on('message', function(msg){
-            switch(msg.type){
-                case 'ChildBlockTemplate':
-                    Object.keys(cluster.workers).forEach(function(id) {
-                        if (cluster.workers[id].type === 'pool'){
-                            cluster.workers[id].send({type: 'ChildBlockTemplate', block: msg.block, poolIndex: msg.poolIndex});
-                        }
-                    });
-                    break;
-                }
-        });
-    };
-
-    var i = 0;
-    var spawnInterval = setInterval(function(){
-        createDaemonWorker(i.toString());
-	i++;
-        if (i === numForks){
-            clearInterval(spawnInterval);
-            log('info', logSystem, 'Child Daemon spawned on %d thread(s)', [numForks]);
-        }
-    }, 10);
-}
-
-
-/**
- * Spawn daemon module
- **/
-function spawnDaemon(){
-    if (!config.poolServer || !config.poolServer.enabled || !config.poolServer.ports || config.poolServer.ports.length === 0) return;
-
-    var worker = cluster.fork({
-        workerType: 'daemon'
-    });
-    worker.on('exit', function(code, signal){
-        log('error', logSystem, 'Daemon died, spawning replacement...');
-        setTimeout(function(){
-            spawnDaemon();
-        }, 10);
-    }).on('message', function(msg){
-        switch(msg.type){
-            case 'BlockTemplate':
-                Object.keys(cluster.workers).forEach(function(id) {
-                    if (cluster.workers[id].type === 'pool'){
-                        cluster.workers[id].send({type: 'BlockTemplate', block: msg.block});
-                    }
-                });
-                break;
-        }
-    });
 }
 
 /**
